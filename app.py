b25ec7c4776548163394c805db0db1144c1d2f8b
danstan5 <35899378+danstan5@users.noreply.github.com>
2018-07-18 00:50:20 +0100

    remove app port

--- app.py
+++ app.py
@@ -772,4 +772,4 @@ def update_figure(selectedData, kdes_df_json):
 #app.css.config.serve_locally = True; app.scripts.config.serve_locally = True
 
 if __name__ == '__main__':
-	app.run_server(debug=True, port=3)
+	app.run_server(debug=True)
